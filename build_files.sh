82da4b46a3ab31e4975fb42655a2ab5c15e0b136
Victor Odhiambo <146205165+radical-vicky@users.noreply.github.com>
2026-09-18 22:34:20 +0300

    Remove redundant echo statements in build script

--- build_files.sh
+++ build_files.sh
@@ -3,10 +3,7 @@ set -o errexit
 
 echo "=== Starting build ==="
 
-echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput --clear
-
-echo "=== Running migrations ==="
 python manage.py migrate --noinput
 
 echo "=== Build complete ==="
