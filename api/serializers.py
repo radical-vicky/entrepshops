cb31ebcb749dc6b80c39b316f1adaf2adf6b1dc7
radical-vicky <entreprenuerridicular@gmail.com>
2026-09-25 08:01:24 +0300

    second commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 
 from store.models import (
     Category, Department, Product, ProductImage, ProductOffer, ProductVariant,
